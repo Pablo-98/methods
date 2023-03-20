d597ad96f6c8c19363c748376bbfea82cc50bd34
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-03-20 13:49:16 -0600

    first

---
+++ method.java
@@ -0,0 +1,27 @@
+public class method{
+    public static void main(String[] args) {
+
+        // hi
+
+        int x = 5;
+
+        int y = 10;
+        printSum(x, y);
+
+
+
+    }
+/*
+write a method named printSum. This method takes two integers: x and y as parameters
+and prints the sum of those two integers 
+
+*/
+    public static void printSum (int one, int two) {
+        // local var
+        int sum = one + two;
+
+        System.out.println(sum);
+
+
+    }
+}
